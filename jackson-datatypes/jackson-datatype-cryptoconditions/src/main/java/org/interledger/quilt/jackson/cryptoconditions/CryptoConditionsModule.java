9e71141f3acf8e00b51c018894a51878c7e83a3e
sappenin <sappenin@gmail.com>
2017-12-19 20:06:01 -0700

    Initial Commit of jackson-datatypes module
    * Add support for serialization/deserialization of Conditions and Fulfillments using Jackson.
    * Configurable Encoding (HEX, Base64, Base64Url, with and without padding)
    * Full unit test coverage.
    
    Signed-off-by: sappenin <sappenin@gmail.com>

---
+++ jackson-datatypes/jackson-datatype-cryptoconditions/src/main/java/org/interledger/quilt/jackson/cryptoconditions/CryptoConditionsModule.java
@@ -0,0 +1,46 @@
+package org.interledger.quilt.jackson.cryptoconditions;
+
+import org.interledger.cryptoconditions.Condition;
+import org.interledger.cryptoconditions.Fulfillment;
+
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
+import java.util.Objects;
+
+/**
+ * A Jackson {@link SimpleModule} for serializing and deserializing Crypto-Condition objects like
+ * {@link Condition} and {@link Fulfillment}.
+ */
+public class CryptoConditionsModule extends SimpleModule {
+
+  private static final String NAME = "CryptoConditionsModule";
+
+  /**
+   * Default Constructor. Specifies an encoding of {@link Encoding#BASE64_WITHOUT_PADDING} by
+   * default.
+   */
+  public CryptoConditionsModule() {
+    this(Encoding.BASE64_WITHOUT_PADDING);
+  }
+
+  /**
+   * Required-args Constructor.
+   *
+   * @param encoding The {@link Encoding} to use for serialization and deserialization of conditions
+   *                 and fulfillments.
+   */
+  public CryptoConditionsModule(final Encoding encoding) {
+    super(
+        NAME,
+        new Version(1, 0, 0, null, "org.interledger", "jackson-datatype-cryptoconditions")
+    );
+
+    Objects.requireNonNull(encoding, "Encoding must not be null!");
+    addSerializer(Condition.class, new ConditionSerializer(encoding));
+    addDeserializer(Condition.class, new ConditionDeserializer(encoding));
+    addSerializer(Fulfillment.class, new FulfillmentSerializer(encoding));
+    addDeserializer(Fulfillment.class, new FulfillmentDeserializer(encoding));
+  }
+
+}
