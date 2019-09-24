17e56674e6c04d9b13c70504c43a3bf02caecc1b
nhartner <nhartner@gmail.com>
2019-09-24 16:31:49 -0600

    javadoc for SpspClient
    Signed-off-by: nhartner <nhartner@gmail.com>

--- spsp-parent/spsp-client/src/main/java/org/interledger/spsp/client/SpspClient.java
+++ spsp-parent/spsp-client/src/main/java/org/interledger/spsp/client/SpspClient.java
@@ -3,10 +3,22 @@ package org.interledger.spsp.client;
 import org.interledger.spsp.PaymentPointer;
 import org.interledger.spsp.StreamConnectionDetails;
 
+/**
+ * Client operations related to SPSP.
+ *
+ * @see "https://github.com/interledger/rfcs/blob/master/0009-simple-payment-setup-protocol/0009-simple-payment-setup-protocol.md"
+ */
 public interface SpspClient {
 
   String ACCEPT_SPSP_JSON = "application/spsp4+json, application/spsp+json";
 
+  /**
+   * Calls SPSP endpoint to obtain address and secret needed to start a stream connection for the give payment pointer.
+   * @see "https://github.com/interledger/rfcs/blob/master/0009-simple-payment-setup-protocol/0009-simple-payment-setup-protocol.md#query-get-spsp-endpoint"
+   * @param paymentPointer payment pointer to query on
+   * @return stream connection details
+   * @throws InvalidReceiverClientException thrown if the payment pointer does not point to a valid account
+   */
   StreamConnectionDetails getStreamConnectionDetails(PaymentPointer paymentPointer)
       throws InvalidReceiverClientException;
 
