863140048eea9de5177db554c20eb2ffff66df3f
David Fuelling <sappenin@gmail.com>
2019-09-26 17:55:14 -0600

    Improve Javadoc
    Signed-off-by: David Fuelling <sappenin@gmail.com>

--- stream-parent/stream-core/src/main/java/org/interledger/stream/frames/ConnectionAssetDetailsFrame.java
+++ stream-parent/stream-core/src/main/java/org/interledger/stream/frames/ConnectionAssetDetailsFrame.java
@@ -50,9 +50,30 @@ public interface ConnectionAssetDetailsFrame extends StreamFrame {
   String sourceAssetCode();
 
   /**
-   * Asset scale of endpoint that sent the frame.
+   * An asset scale is the difference, in orders of magnitude, between an asset's `standard unit` and a corresponding
+   * `fractional unit`.
    *
-   * @return A {@link Short}.
+   * <p>A standard unit represents the typical unit of account for a particular asset. For example 1 USD in the case of
+   * U.S. dollars, or 1 BTC in the case of Bitcoin (Note that peers are free to define this value in any way, but
+   * participants in an Interledger accounting relationship must be sure to use the same value. Thus, it is suggested to
+   * use typical values when possible).</p>
+   *
+   * <p>A fractional unit represents some unit smaller than its corresponding standard unit, but with greater
+   * precision. Examples of fractional monetary units include one cent ($0.01 USD), or 1 satoshi (0.00000001 BTC).</p>
+   *
+   * <p>Because Interledger amounts are integers, but most currencies are typically represented as fractional units
+   * (e.g. cents), this property defines how many Interledger units make up one standard unit of the asset code
+   * specified above.</p>
+   *
+   * <p>More formally, the asset scale is a non-negative integer (0, 1, 2, …) such that one standard unit equals
+   * 10^(-scale) of a corresponding fractional unit. If the fractional unit equals the standard unit, then the asset
+   * scale is 0.</p>
+   *
+   * <p>For example, one "cent" represents an asset scale of 2 in the case of USD; 1 satoshi represents an asset scale
+   * of 8 in the case of Bitcoin; and 1 drop represents an asset scale of 6 in XRP. For dollars, this would usually be
+   * set to 9, so that Interledger # amounts are expressed in nano-dollars.</p>
+   *
+   * @return A {@link Short} representing the asset scale.
    */
   short sourceAssetScale();
 
