26af15d954a261cf927ed8341ca4ed463e5d4152
David Fuelling <sappenin@gmail.com>
2021-01-24 12:01:28 -0700

    Fix javadoc
    Signed-off-by: David Fuelling <sappenin@gmail.com>

--- stream-parent/stream-pay/src/main/java/org/interledger/stream/pay/model/Quote.java
+++ stream-parent/stream-pay/src/main/java/org/interledger/stream/pay/model/Quote.java
@@ -1,8 +1,8 @@
 package org.interledger.stream.pay.model;
 
 import org.interledger.core.fluent.Ratio;
-import org.interledger.stream.model.AccountDetails;
 import org.interledger.stream.connection.StreamConnection;
+import org.interledger.stream.model.AccountDetails;
 import org.interledger.stream.pay.model.ImmutableQuote.Builder;
 import org.interledger.stream.pay.probing.model.EstimatedPaymentOutcome;
 import org.interledger.stream.pay.probing.model.ExchangeRateProbeOutcome;
@@ -30,17 +30,23 @@ public interface Quote {
   PaymentOptions paymentOptions();
 
   /**
-   * Source account details
+   * Source account details.
+   *
+   * @return An {@link AccountDetails}.
    */
   AccountDetails sourceAccount();
 
   /**
-   * Destination account details
+   * Destination account details.
+   *
+   * @return An {@link AccountDetails}.
    */
   AccountDetails destinationAccount();
 
   /**
    * Minimum exchange rate allowed for a payment; used to enforce rates.
+   *
+   * @return A {@link Ratio}.
    */
   Ratio minAllowedExchangeRate();
 
@@ -61,11 +67,15 @@ public interface Quote {
   /**
    * Probed exchange rate over the path, as a range between {@link ExchangeRateProbeOutcome#lowerBoundRate} and {@link
    * ExchangeRateProbeOutcome#upperBoundRate}.
+   *
+   * @return A {@link ExchangeRateProbeOutcome}.
    */
   ExchangeRateProbeOutcome estimatedExchangeRate();
 
   /**
-   * Estimated payment duration in milliseconds, based on max packet amount, RTT, and rate of packet throttling
+   * Estimated payment duration in milliseconds, based on max packet amount, RTT, and rate of packet throttling.
+   *
+   * @return A {@link Duration}.
    */
   Duration estimatedDuration();
 
@@ -73,7 +83,7 @@ public interface Quote {
    * Estimated information about the ultimate payment outcome, as computed from the details discovered in {@link
    * ExchangeRateProbeOutcome}.
    *
-   * @return
+   * @return A {@link EstimatedPaymentOutcome}.
    */
   EstimatedPaymentOutcome estimatedPaymentOutcome();
 }
